6cc707a6e3bf591be69a41b4e261f6fa12e9d3a8
Dimitrisl <dlolis88@gmail.com>
2017-01-14 18:25:47 +0200

    both ready

--- DatabaseAssignment/src/Bothnlj.java
+++ DatabaseAssignment/src/Bothnlj.java
@@ -5,28 +5,44 @@ import java.io.IOException;
 
 public class Bothnlj {
 	
-	public static void joinstart(String file1,String file2,int m,int cattribute1,int cattribute2) throws IOException//filepath1,filepath2,memory size,collumnf1,collumnf2
+	public static void joinstart(String file1,String file2,int memory,int cattribute1,int cattribute2) throws IOException//filepath1,filepath2,memory size,collumnf1,collumnf2
 	{
 
 		int numberoflines1 = ReadingWritingFile.readFirstLineofFile(file1);
 		int numberoflines2 = ReadingWritingFile.readFirstLineofFile(file2);
 		String file1line = "";
 		String file2line = "";
-		try {
-			BufferedReader fr = new BufferedReader(new FileReader(file1));
-			file1line = fr.readLine();
-		} catch (FileNotFoundException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
+		int[] memo1 = new int[numberoflines1];
+		int[] memo2 = new int[numberoflines2];
+		BufferedReader fr = new BufferedReader(new FileReader(file1));
+		file1line = fr.readLine();
+		BufferedReader sr = new BufferedReader(new FileReader(file2));
+		file2line = sr.readLine();
+		
+		while(((file2line = sr.readLine())!=null))	
+		{
+			int line2 = Integer.parseInt(file2line.split(",")[cattribute2]);
+			while (((file1line =fr.readLine())!=null))
+			{
+				int line1 = Integer.parseInt(file1line.split(",")[cattribute1]);
+				 if (line1 == line2)
+				 {
+					 String concatenated_string = "";
+					 for (int counter = 0;counter<file1line.split(",").length;counter++)
+					 {
+						 if (counter == cattribute1)
+						 {
+							 continue;
+						 }else
+						 {
+							 concatenated_string +=file1line.split(",")[counter];
+						 }
+					 }
+					 concatenated_string +=file2line;
+					 ReadingWritingFile.Writethis(concatenated_string);
+				 }
+			}
 		}
-		try {
-			 BufferedReader sr = new BufferedReader(new FileReader(file2));
-			 file1line = sr.readLine();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		 
 	}
 }
 
