7ec70faf3c69552302f88cbf17c0e308fdc20a2b
Dimitrisl <dlolis88@gmail.com>
2017-01-15 22:23:52 +0200

    fixed sorting

--- DatabaseAssignment/src/SMJ.java
+++ DatabaseAssignment/src/SMJ.java
@@ -1,7 +1,17 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
 
 public class SMJ {
 	
-	public static void smjjoinstart(String f1, String f2, int memsize, int attribute1, int attribute2){
+	public static void main(String[] args) throws IOException
+	{
+		smjjoinstart("A.csv","B.csv",50,1,2);
+	}
+	
+	
+	public static void smjjoinstart(String f1, String f2, int memsize, int attribute1, int attribute2) throws IOException{
 		int numberoflines1 = ReadingWritingFile.readFirstLineofFile(f1);
 		int numberoflines2 = ReadingWritingFile.readFirstLineofFile(f2);
 		String lines1 = "";
@@ -12,52 +22,45 @@ public class SMJ {
 		int j=1;
 		String []text1 = new String[numberoflines1];
 		String []text2 = new String[numberoflines2];
-		
-		text1 = sortedbyattribute(text1,attribute1);
-		text2 = sortedbyattribute(text2,attribute2);
-		
-		while (i<=numberoflines1 && j<=numberoflines2){
-			
+	
+		BufferedReader fr=new BufferedReader(new FileReader(f1));
+		fr.readLine();
+		for (int y=0;y<numberoflines1;y++)
+		{
+			text1[y]=fr.readLine();
 		}
+		
+		
+		text1 = sortedbyattribute(text1,attribute1,numberoflines1);// sorted file1
+		text2 = sortedbyattribute(text2,attribute2,numberoflines2);// sorted file2
+		
+	
+		
+		
+		for (int counter=0;counter<text1.length;counter++)
+		{
+			text1[counter] =  text1[counter].replace((text1[counter].split("A")[0])+"A","");
+			System.out.println(text1[counter]);
+		}
+
 	}
 	
 	public static void OutputTuples(int i, int j, int numberoflines1, int numberoflines2){
 		
 	}
 
-	public String[] sortedbyattribute(String[] file,int attribute1)
+	public static String[] sortedbyattribute(String[] file,int attribute1,int numberoflines)
 	{
-		String[] line;
-		String[] resta;
+		String []line = new String[numberoflines];
+		
 		for (int i=0;i<file.length;i++)
 		{
-			for (int j=0)
-			
-			line[i] = file[i].split(",")[attribute1];
-			
+			line[i] = file[i].split(",")[attribute1].toString()+"A"+file[i];
 		}
-		return Answer; 
+		Arrays.sort(line);
+		return line; 
 	}
 
-	    public static void bubble_srt(int array[]) {
-	        int n = array.length;
-	        int k;
-	        for (int m = n; m >= 0; m--) {
-	            for (int i = 0; i < n - 1; i++) {
-	                k = i + 1;
-	                if (array[i] > array[k]) {
-	                    swapNumbers(i, k, array);
-	                }
-	            }
-	        }
-	    }
-	  
-	    private static void swapNumbers(int i, int j, int[] array) {
-	  
-	        int temp;
-	        temp = array[i];
-	        array[i] = array[j];
-	        array[j] = temp;
-	    }
 
 }
+	
