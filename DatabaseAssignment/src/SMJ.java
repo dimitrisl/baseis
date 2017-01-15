d1a6a8f4d8a6d78aafdb898045968c083de6126c
vasiapars <vasiliki.chira@gmail.com>
2017-01-15 20:09:40 +0200

    smj

---
+++ DatabaseAssignment/src/SMJ.java
@@ -0,0 +1,24 @@
+
+public class SMJ {
+	
+	public static void smjjoinstart(String f1, String f2, int memsize, int attribute1, int attribute2){
+		int numberoflines1 = ReadingWritingFile.readFirstLineofFile(f1);
+		int numberoflines2 = ReadingWritingFile.readFirstLineofFile(f2);
+		String lines1 = "";
+		String lines2 = "";
+		int []mem = new int[memsize];
+		
+		 
+		
+		int i=1;
+		int j=1;
+		while (i<=numberoflines1 && j<=numberoflines2){
+			
+		}
+	}
+	
+	public static void OutputTuples(int i, int j, int numberoflines1, int numberoflines2){
+		
+	}
+
+}
