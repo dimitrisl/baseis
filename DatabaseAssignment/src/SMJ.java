c2f00eb147e8903f293160c60da59fb6546a7cd5
dimitrisl <dlolis88@gmail.com>
2017-01-16 20:00:50 +0200

    Update SMJ.java
    changes in smj

--- DatabaseAssignment/src/SMJ.java
+++ DatabaseAssignment/src/SMJ.java
@@ -1,52 +1,61 @@
-import java.io.BufferedReader;
+	import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class SMJ {
 	
 	public static void main(String[] args) throws IOException
 	{
-		smjjoinstart("D.csv","E.csv",50,1,2);
+		smjjoinstart("test.csv","test3.csv",10,1,2);
 	}
 	
 	
 	public static void smjjoinstart(String f1, String f2, int memsize, int attribute1, int attribute2) throws IOException{
 		int numberoflines1 = ReadingWritingFile.readFirstLineofFile(f1);
 		int numberoflines2 = ReadingWritingFile.readFirstLineofFile(f2);
-		String lines1 = "";
-		String lines2 = "";
-		int []mem = new int[memsize];
-			 	
-		int i=1;
-		int j=1;
-		String []text1 = new String[numberoflines1];
-		String []text2 = new String[numberoflines2];
-	
 		BufferedReader fr=new BufferedReader(new FileReader(f1));
 		fr.readLine();
-		for (int y=0;y<numberoflines1;y++)
-		{
-			text1[y]=fr.readLine();
-		}
-		
 		BufferedReader sr=new BufferedReader(new FileReader(f2));
 		sr.readLine();
-		for (int y=0;y<numberoflines2;y++)
+		String line = fr.readLine();
+		String []memory = new String[memsize];
+		
+		
+		while (line!=null)
 		{
-			text2[y]=sr.readLine();
+			for (int i=0;i<memory.length;i++)
+			{
+				if (line==null)
+				{
+					memory[i] = "Z";
+				}else
+				{
+					memory[i] = line ;
+				}
+				line = fr.readLine();
+			}
+//			String []placeholder = new String [memsize];
+//			int counter = 0;
+//			while (memory[counter]!="EMPTY")
+//				{
+//					placeholder[counter] = memory[counter];
+//					counter++;
+//				}
+//			placeholder = sortedbyattribute(placeholder,attribute1,memsize);
+//			
+			memory =sortedbyattribute(memory,attribute1,memsize);
+			
+			
 		}
-		
-		text1 = sortedbyattribute(text1,attribute1,numberoflines1);// sorted file1
-		text2 = sortedbyattribute(text2,attribute2,numberoflines2);// sorted file2
-		
-		
+
 		//from now on we have to take some of the records of text1
 		//to the temporary file.
-		for (int counter=0;counter<text1.length;counter++)
-		{
-			System.out.println(text1[counter]);
-		}
+//		for (int counter=0;counter<memory.length;counter++)
+//		{
+//			System.out.println(memory[counter]);
+//		}
 
 	}
 	
@@ -60,14 +69,25 @@ public class SMJ {
 		
 		for (int i=0;i<file.length;i++)
 		{
-			line[i] = file[i].split(",")[attribute1].toString()+"A"+file[i];
+			if (file[i]=="Z")
+			{
+				line[i]=null;
+			}
+			else
+			{
+				line[i] = file[i].split(",")[attribute1].toString()+"A"+file[i];
+			}
 		}
-		Arrays.sort(line);
-		
+		//Arrays.sort(line);
+		Arrays.sort(line, Comparator.nullsLast(String.CASE_INSENSITIVE_ORDER));
+
 		for (int counter=0;counter<line.length;counter++)
 		{
-			line[counter] =  line[counter].replace((line[counter].split("A")[0])+"A","");
-			System.out.println(line[counter]);
+			if (line[counter]!=null)
+			{	
+				line[counter] =  line[counter].replace((line[counter].split("A")[0])+"A","");
+				System.out.println(line[counter]);
+			}
 		}
 		
 		return line; 
