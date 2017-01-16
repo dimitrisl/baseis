f2106fa257324a870e45fc616b3bb78969754dec
ChrisChatz <chrischatzilenas@gmail.com>
2017-01-16 12:58:53 +0200

    changes in NoneNlj

--- DatabaseAssignment/src/NoneNlj.java
+++ DatabaseAssignment/src/NoneNlj.java
@@ -16,21 +16,13 @@ public class NoneNlj {
 		numOfLines1=ReadingWritingFile.readFirstLineofFile(filepath1);
 		numOfLines2=ReadingWritingFile.readFirstLineofFile(filepath2);
 		
-		/*if(numOfLines1>=numOfLines2){//put the file with less records in the outer loop
-			swapIntegers(numOfLines1,numOfLines2);
-			swapStrings(filepath1,filepath2);
-			swapIntegers(attributesF1,attributesF2);
-		}*/
-		
 		FileReader fr1=new FileReader(filepath1);
 		FileReader fr2=new FileReader(filepath2);
-		FileReader fr3=new FileReader(filepath1);
+
 		BufferedReader fr=new BufferedReader(fr1);
 		BufferedReader sr=new BufferedReader(fr2);
-		BufferedReader outRead=new BufferedReader(fr3);
 	
 		sr.readLine();
-		outRead.readLine();
 		
 		
 		while ((lines1 = fr.readLine()) != null)
@@ -42,6 +34,9 @@ public class NoneNlj {
 				for(int k=0;k<memsize-1;k++){
 					lines1=fr.readLine();
 					if(lines1==null){
+						for(int z=k;z<memsize-1;z++){
+							memory[z]=0;
+						}
 						break;
 					}
 					memlines[k]=lines1;
@@ -55,56 +50,22 @@ public class NoneNlj {
 					memory[memsize-1]=Integer.parseInt(lines2.split(",")[attributesF2]);//change in every iteration only the last record in the memory
 					for(int l=0;l<memsize-1;l++){
 						if(memory[l]==memory[memsize-1]){
-							System.out.println(memlines[l]+","+memlines[memsize-1]);
-							ReadingWritingFile.Writethis(memlines[l]+","+memlines[memsize-1]);
-							
+							String temp=memlines[l].split(",")[attributesF1];
+							memlines[l]=memlines[l].replaceAll(temp+",","");
+							ReadingWritingFile.Writethis(memlines[l]+","+memlines[memsize-1]+"\n");
 						}
 							
 					}
 							
-				
-					/*//print memory
-					for(int m=0;m<memsize;m++){
-						System.out.println(m+" "+memory[m]);
-						System.out.println(m+" "+memlines[m]);
-					}*/
-					
-
 				}
 				sr.close();
 				sr = new BufferedReader(new FileReader(filepath2));
-				sr.readLine();
-				
-				
-								
+				sr.readLine();							
 				
 			}
 		}
-		outRead.close();
 		fr.close();
 		sr.close();
-	}
-	
-	public static void swapIntegers(int a,int b){
-	    int temp = a;
-	    a = b;
-	    b = temp;
-	}
-	public static void swapStrings(String a,String b){
-	    String temp = a;
-	    a = b;
-	    b = temp;
-	}
-	
-	public static void main(String[] args) {
-		String file1="test.csv";
-		String file2="test3.csv";
-		try {
-			joinstart(file1,file2 , 2, 0, 1);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 	}
 	
 }
