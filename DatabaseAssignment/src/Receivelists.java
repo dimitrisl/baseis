089100fa50e4b8151d63b5c47ada38fedaedfe9b
Dimitrisl <dlolis88@gmail.com>
2017-01-17 16:53:26 +0200

    phase2 start

---
+++ DatabaseAssignment/src/Receivelists.java
@@ -0,0 +1,17 @@
+
+public class Receivelists {
+
+	public void reclists(String file,int numfiles,int attribute,int memorysize)
+	{
+			if (numfiles<=memorysize)
+			{
+			String []Temfiles = new String[numfiles];// we have an array of strings that contain the temp files of each sorted sublist
+			
+				for (int i= 0 ;i<Temfiles.length;i++)
+				{
+					
+				}
+			
+		}
+	}
+}
